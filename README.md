336940252a24d0d50e3ffc88f7140c35723313dd
vpatoka <vlad.patoka@opsguru.io>
2024-01-24 19:32:30 -0500

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,169 @@
-# NeoPixelPi
-Neopixel on Rapbery Pi
+# Update Your Pi and Python
+
+Run the standard updates:
+
+```
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3-pip
+```
+
+and upgrade setuptools:
+
+```
+sudo apt install --upgrade python3-setuptools
+```
+
+Python2 support has been dropped, so you will need to either use pip3 and python3 as commands or set Python 3 as the default python install.
+
+You may need to reboot prior to installing Blinka. The raspi-blinka.py script will inform you if it is necessary.
+
+If you are installing in a virtual environment, the installer may not work correctly since it requires sudo. We recommend using pip to manually install it in that case.
+
+# Setup Virtual Environment
+
+If you are installing on the Bookworm version of Raspberry Pi OS, you will need to install your python modules in a virtual environment. You can find more information in the [Python Virtual Environment Usage on Raspberry Pi](https://learn.adafruit.com/python-virtual-environment-usage-on-raspberry-pi) guide. To Install and activate the virtual environment, use the following commands:
+
+```
+sudo apt install python3.11-venv
+python -m venv env --system-site-packages
+```
+
+You will need to activate the virtual environment every time the Pi is rebooted. To activate it:
+
+```
+source env/bin/activate
+```
+
+To deactivate, you can use `deactivate`, but leave it active for now.
+
+# Automated Install
+
+Creators put together a script to easily make sure your Pi is correctly configured and install Blinka. It requires just a few commands to run. Most of it is installing the dependencies.
+
+```
+cd ~
+pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo -E env PATH=$PATH python3 raspi-blinka.py
+```
+
+If you are installing on an earlier version such as Bullseye of Raspberry Pi OS, you can call the script like:
+
+```
+sudo python3 raspi-blinka.py
+```
+
+
+
+It may take a few minutes to run. When it finishes, reboot the Raspberry Pi
+
+
+
+Once it reboots, the connection will close. After a couple of minutes, you can reconnect.
+
+# Check I2C and SPI
+
+The script will automatically enable I2C and SPI. You can run the following command to verify:
+
+[ Copy Text](https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi#)
+
+```
+ls /dev/i2c* /dev/spi*
+```
+
+You should see the response
+
+```
+/dev/i2c-1 /dev/spidev0.0 /dev/spidev0.1
+```
+
+
+
+
+
+# Blinka Test
+
+Create a new file called **blinkatest.py** with **nano** or your favorite text editor and put the following in:
+
+```python
+import board
+import digitalio
+import busio
+
+print("Hello blinka!")
+
+# Try to great a Digital input
+pin = digitalio.DigitalInOut(board.D4)
+print("Digital IO ok!")
+
+# Try to create an I2C device
+i2c = busio.I2C(board.SCL, board.SDA)
+print("I2C ok!")
+
+# Try to create an SPI device
+spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
+print("SPI ok!")
+
+print("done!")
+```
+
+Save it and run at the command line with:
+
+
+
+```
+python3 blinkatest.py
+```
+
+You should see its goes through, indicating digital i/o, I2C and SPI all worked.
+
+# Python Installation of NeoPixel Library
+
+As Adafruit_Blinka library has been installed, it is time to install NeoPixel:
+
+- `source env/bin/activate`
+
+- `pip3 install rpi_ws281x adafruit-circuitpython-neopixel`
+
+  
+
+If all this steps are done, we could connect WS281x strip to Raspberry PI, using +5V and Ground pins for the power and Pin D18 for Data
+*(!)* NOTE - We have to use LED Signal Amplifier to avoid direct connection of LED to Raspberry PI. I am using `SP901E` model.
+
+There is few effects included to the code. Also it has a logic to start the show 30 minutes after sunset and stop it 30 minutes before the sunrise.
+
+# Make Python code to run as a service
+
+To make live easier, we have to create custom service to run it every time we boot the Raspberry PI host
+
+- `systemctl edit --force --full led_controller.service`
+
+  The content could be something like this:
+  
+
+  **`[Unit]`**
+  **`Description=Led Controller Service`**
+  **`Wants=network.target`**
+  **`After=network.target`**
+
+  **`[Service]`**
+  **`ExecStartPre=/bin/sleep 10`**
+  **`ExecStart=/usr/local/bin/led_controller.py`**
+  **`Restart=always`**
+
+  **`[Install]`**
+  **`WantedBy=multi-user.target`**
+
+  
+
+- `systemcrl list`
+
+- `systemctl status led_controller.service`
+
+- `systemctl enable led_controller.service`
+
+- `systemctl enable led_controller.service`
+
+  
+
---
+++ led_controller/led_controller.py
@@ -0,0 +1,431 @@
+#!/root/env/bin/python3
+# -*- coding: utf-8 -*
+
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# NeoPixels on Raspberry Pi
+
+# Credits to https://www.tweaking4all.com/hardware/arduino/adruino-led-strip-effects/ 
+
+# Made by me, Vlad, just for fun
+
+import time
+import board
+import neopixel
+import adafruit_fancyled.adafruit_fancyled as fancy
+
+import math
+import random
+import logging
+from time import sleep
+from datetime import date, datetime, timezone, timedelta
+from suntime import Sun, SunTimeException
+
+
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 420
+
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.GRB
+
+# Colors
+RED = (255, 0, 0)
+ORANGE = (255, 128, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+LIGHT_BLUE = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (127, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Declare a 6-element RGB rainbow palette
+palette = [ 
+  fancy.CRGB(1.0, 0.0, 0.0),  # Red 
+  fancy.CRGB(0.5, 0.5, 0.0),  # Yellow
+  fancy.CRGB(0.0, 1.0, 0.0),  # Green
+  fancy.CRGB(0.0, 0.5, 0.5),  # Cyan
+  fancy.CRGB(0.0, 0.0, 1.0),  # Blue
+  fancy.CRGB(0.5, 0.0, 0.5),  # Magenta
+]
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+
+def wheel(pos):
+  # Input a value 0 to 255 to get a color value.
+  # The colours are a transition r - g - b - back to r.
+  if pos < 0 or pos > 255:
+    r = g = b = 0
+  elif pos < 85:
+    r = int(pos * 3)
+    g = int(255 - pos * 3)
+    b = 0
+  elif pos < 170:
+    pos -= 85
+    r = int(255 - pos * 3)
+    g = 0
+    b = int(pos * 3)
+  else:
+    pos -= 170
+    r = 0
+    g = int(pos * 3)
+    b = int(255 - pos * 3)
+  return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
+def setPixelHeatColor(Pixel, temperature):
+  # Scale 'heat' down from 0-255 to 0-191
+  t192 = round((temperature/255.0)*191)
+
+  # calculate ramp up from
+  heatramp = t192 & 0x3F # 0..63
+  heatramp <<= 2 # scale up to 0..252
+  
+  # figure out which third of the spectrum we're in
+  if t192 > 0x80 :
+    pixels[Pixel] = (255, 255, heatramp) # hottest
+  elif t192 > 0x40 :
+    pixels[Pixel] = (255, heatramp, 0)   # medium
+  else :
+    pixels[Pixel] = (heatramp, 0, 0)     # coolest
+
+    
+def rainbow_cycle():
+  for l in range(255):
+    for j in range(255):
+      for i in range(num_pixels):
+        pixel_index = int((i * 256 // num_pixels) + j)
+        pixels[i] = wheel(pixel_index & 255)
+      pixels.show()
+      time.sleep(0.001) # rainbow cycle with 1ms delay per step
+
+
+def black_and_white():
+  for i in range(0,num_pixels-29,30):
+    for b in range(8):
+      pixels[i+b] = BLACK
+    for b in range(8,22):
+      if 12 < b < 17 :
+        pixels[i+b] = BLACK
+      else:
+        pixels[i+b] = WHITE
+    for b in range(22,30):
+      pixels[i+b] = BLACK
+
+
+def holiday():
+  for i in range(0,num_pixels-29,30):
+    for b in range(0,8):
+      pixels[i+b] = GREEN
+    for b in range(8,22):
+      if 12 < b < 17 :
+        pixels[i+b] = GREEN
+      else:
+        pixels[i+b] = RED
+    for b in range(22,30):
+      pixels[i+b] = GREEN
+
+
+#random.uniform(0, 1)
+def halloween():
+  for l in range(255):
+    if bool(random.getrandbits(1)):
+      for i in range(0,num_pixels-29,30):
+       for b in range(8):
+         pixels[i+b] = BLACK
+       for b in range(8,22):
+         if b > 12 and b < 17 :
+           pixels[i+b] = BLACK
+         else:
+           pixels[i+b] = ORANGE
+       for b in range(22,30):
+         pixels[i+b]  = BLACK
+    else:
+      pixels.fill(BLACK)
+    pixels.show()
+    sleep(random.uniform(0.09, 0.5))
+
+
+def gradient():
+  offset = 0
+  for l in range(255):
+    for i in range(num_pixels):
+      # Load each pixel's color from the palette using an offset, run it
+      # through the gamma function, pack RGB value and assign to pixel.
+      color = fancy.palette_lookup(palette, offset + i / num_pixels)
+      color = fancy.gamma_adjust(color, brightness=0.25)
+      pixels[i] = color.pack()
+    pixels.show()
+    offset += 0.02 # Bigger number = faster spin
+
+
+def knight_rider(color, wait, barLength):
+  off = BLACK
+  for l in range(255):
+    i = j = k = 0
+    while i < num_pixels:
+      for k in range(barLength):
+        pixels[k] = color
+        pixels.show()
+        sleep(wait)
+      for j in range(num_pixels-barLength):
+        pixels[j] = off
+        pixels[j+barLength] = color
+        pixels.show()
+        sleep(wait)
+      for k in range(num_pixels-1,num_pixels-barLength,-1):
+        pixels[k] = color
+        pixels.show()
+        sleep(wait)
+      for j in range(num_pixels-1,0,-1):
+        pixels[j] = off
+        pixels[j-barLength] = color
+        pixels.show()
+        sleep(wait)
+    pixels[0] = off
+    pixels.show()
+    sleep(wait)
+
+
+def RGBLoop():
+  for l in range(255):
+    for j in range(0,3):
+      for k in range(0,255): # Fade IN
+        match j:
+          case 0:
+            pixels.fill((k, 0, 0))
+          case 1:
+            pixels.fill((0, k, 0))
+          case 2:
+            pixels.fill((0, 0, k))
+        pixels.show()
+        sleep(3)
+      for k in range(255,0,-1): # Fade OUT
+        match j:
+          case 0:
+            pixels.fill((k, 0, 0))
+          case 1:
+            pixels.fill((0, k, 0))
+          case 2:
+            pixels.fill((0, 0, k))
+        pixels.show()
+        sleep(3)
+
+
+def CylonBounce(r, g, b, EyeSize, SpeedDelay, ReturnDelay):
+  for l in range(255):
+    for i in range(0,num_pixels-EyeSize-2):
+      pixels.fill(BLACK)
+      pixels[i]  = (int(r/10), int(g/10), int(b/10))
+      for j in range(1,EyeSize+1):
+        pixels[i+j]  = (r, g, b)
+      pixels[i+EyeSize+1]  = (int(r/10), int(g/10), int(b/10))
+      pixels.show()
+      sleep(SpeedDelay)
+
+    sleep(ReturnDelay)
+
+    for i in range(num_pixels-EyeSize-2,0, -1):
+      pixels.fill(BLACK)
+      pixels[i]  = (int(r/10), int(g/10), int(b/10))
+      for j in range(1,EyeSize+1):
+        pixels[i+j]  = (r, g, b)
+      pixels[i+EyeSize+1]  = (int(r/10), int(g/10), int(b/10))
+      pixels.show()
+      sleep(SpeedDelay)
+
+    sleep(ReturnDelay)
+
+
+def Twinkle(r, g, b, Count, SpeedDelay, OnlyOne):
+  for l in range(255):
+    pixels.fill(BLACK)
+    for i in range(0,Count):
+      pixels[random.randint(0,num_pixels-1)]  = (r, g, b)
+      pixels.show()
+      sleep(SpeedDelay)
+      if OnlyOne:
+        pixels.fill(BLACK)
+    sleep(SpeedDelay)
+
+
+def TwinkleRandom(Count, SpeedDelay, OnlyOne):
+  for l in range(255):
+    pixels.fill(BLACK)
+    for i in range(0,Count):
+      pixels[random.randint(0,num_pixels-1)]  = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+      pixels.show()
+      sleep(SpeedDelay)
+      if OnlyOne:
+        pixels.fill(BLACK)
+    sleep(SpeedDelay)
+
+
+def Sparkle(r, g, b, SpeedDelay):
+  for l in range(255):
+    Pixel = random.randint(0,num_pixels-1)
+    pixels[Pixel] = (r, g, b)
+    pixels.show()
+    sleep(SpeedDelay)
+    pixels[Pixel] = (0,0,0)
+
+
+def SnowSparkle(r, g, b, SparkleDelay, SpeedDelay):
+  for l in range(255):
+    pixels.fill((r, g, b))
+    Pixel = random.randint(0,num_pixels-1)
+    pixels[Pixel] = (255,255,255)
+    pixels.show()
+    sleep(SparkleDelay)
+    pixels[Pixel] = ((r, g, b))
+    pixels.show()
+    sleep(SpeedDelay)
+
+
+def RunningLights(r, g, b, WaveDelay):
+  for l in range(128):
+    Position = 0
+    for j in range(num_pixels*2):
+      Position += 1
+      for i in range(num_pixels-1):
+        # sine wave, 3 offset waves make a rainbow!
+        # float level = sin(i+Position) * 127 + 128;
+        # setPixel(i,level,0,0);
+        # float level = sin(i+Position) * 127 + 128;
+        R = int(((math.sin(i+Position) * 127 + 128)/255)*r)
+        G = int(((math.sin(i+Position) * 127 + 128)/255)*g)
+        B = int(((math.sin(i+Position) * 127 + 128)/255)*b)
+        pixels[i] = (R, G, B)
+      pixels.show()
+      sleep(WaveDelay)
+
+
+def colorWipe(r, g, b, SpeedDelay):
+  for l in range(255):
+    for i in range(num_pixels-1):
+      pixels[i] = (r, g, b)
+      pixels.show()
+      sleep(SpeedDelay)
+    for i in range(num_pixels-1):
+      pixels[i] = (0, 0, 0)
+      pixels.show()
+      sleep(SpeedDelay)
+
+
+def theaterChase(r, g, b, SpeedDelay):
+  for l in range(255):
+    for j in range(0,9): # do 10 cycles of chasing
+      for q in range (0,2):
+        for i in range(0, num_pixels-1, 3):
+          pixels[i+q] = (r, g, b) # turn every third pixel on
+        pixels.show()
+        sleep(SpeedDelay)
+        for i in range(0, num_pixels-1, 3):
+          pixels[i+q] = (0, 0, 0) # turn every third pixel off
+
+
+def theaterChaseRainbow(SpeedDelay):
+  for l in range(255):
+    for j in range(0,255): # cycle all 256 colors
+      for q in range (0,2):
+        for i in range(0, num_pixels-1, 3):
+          pixels[i+q] = wheel( (i+j) % 255 ) # turn every third pixel on
+        pixels.show()
+        sleep(SpeedDelay)
+        for i in range(0, num_pixels-1, 3):
+          pixels[i+q] = (0, 0, 0) # turn every third pixel off
+
+
+def Fire(Cooling, Sparking, SpeedDelay):
+  for l in range(255):
+
+    heat = [0] * num_pixels
+
+    for i in range(0, num_pixels-1):
+      cooldown = random.randint(0, int(((Cooling * 10) / num_pixels) + 2))
+      if cooldown > heat[i]:
+        heat[i] = 0
+      else:
+        heat[i]=heat[i]-cooldown
+
+    for k in range(num_pixels-2, 2, -1):
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3
+
+    if random.randint(1, 255) < Sparking:
+      y = random.randint(1, 7)
+      heat[y] = heat[y] + random.randint(160,255)
+
+    for j in range(0, num_pixels-1):
+       setPixelHeatColor(j, heat[j])
+
+    pixels.show()
+    sleep(SpeedDelay)
+
+
+#####################################################################################
+# Main loop
+
+try:
+
+  latitude = 75.0
+  longitude = 0
+  sun = Sun(latitude, longitude)
+  current_month_day = datetime.now().strftime('%m-%d')
+
+  pixels.fill((0,0,0))
+  pixels.show()
+
+  while True:
+
+    today_sr = sun.get_sunrise_time().astimezone()
+    today_ss = sun.get_sunset_time().astimezone()
+
+    now = datetime.now().astimezone()
+
+    # Run only if time is 30 minute before sunrise and 30 minutes afetr sunset
+    if ((today_sr - timedelta(minutes=30)) < now < (today_ss + timedelta(minutes=30))):
+      pixels.fill((0, 0, 0)) # All black - not time to iluminate
+    else:
+      match current_month_day:
+        case '12-15'|'12-16'|'12-17'|'12-18'|'12-19'|'12-20'|'12-21'|'12-22'|'12-23'|'12-26'|'12-27'|'12-28'|'12-29'|'12-30':
+          holiday()
+        case '01-02'|'01-03'|'01-04'|'01-05'|'01-06'|'01-07'|'01-08':
+          holiday()
+        case '12-24'|'12-25'|'12-31'|'01-01':
+          rainbow_cycle()
+        case '01-31'|'06-15'|'07-30'|'10-26':
+          gradient()
+        case '02-02':
+          knight_rider(RED, 0.05, 8)
+        case '10-31':
+          halloween()
+        case _:
+          #RGBLoop()
+          #CylonBounce(0xff, 0, 0, 4, 0.010, 0.050)
+          #Twinkle(255, 0, 0, 3, 0.100, False) # Width = 3 pixels
+          #TwinkleRandom(2, 0.100, False) # Width = 2 pixels
+          #Sparkle(255,255,255,0)
+          #SnowSparkle(16, 16, 16, 0.020, random.randint(100,1000))
+          #RunningLights(0, 255, 0, 0.050)
+          #colorWipe(0, 255, 0, 0.050)
+          #theaterChase(255,0,0,0.050)
+          #theaterChaseRainbow(0.050)
+          #Fire(55,120,0.15)
+          black_and_white()
+
+    pixels.show()
+    sleep(600)
+
+except KeyboardInterrupt as e:
+  logging.info("Stopping...")
+
+
